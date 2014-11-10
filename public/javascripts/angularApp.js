138e617a781722c6c87592c24bd8fc3d8f588fab
Yong Li <yong@global-initiative.com>
2014-11-10 17:18:55 +0000

    The Posts Page - Creating New Comments

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -65,4 +65,14 @@ function($scope, $stateParams, posts){
 'posts',
 function($scope, $stateParams, posts){
 	$scope.post = posts.posts[$stateParams.id];
+	
+	$scope.addComment = function(){
+	  if($scope.body === '') { return; }
+	  $scope.post.comments.push({
+	    body: $scope.body,
+	    author: 'user',
+	    upvotes: 0
+	  });
+	  $scope.body = '';
+	};
 }]);
