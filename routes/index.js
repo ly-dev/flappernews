51409b0e91990b48cfee81a53ee17e084f8a6c01
Yong Li <dev@liyong.cn>
2014-11-11 09:37:18 +0000

    Opening REST Routes - Pre-loading Objects

--- routes/index.js
+++ routes/index.js
@@ -34,4 +34,27 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+/* Preload post objects*/
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) {
+    	return next(err); 
+	}
+    
+    if (!post) {
+    	return next(new Error("can't find post")); 
+	}
+
+    req.post = post;
+    return next();
+  });
+});
+
+/* Load a single post */
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+
 module.exports = router;
